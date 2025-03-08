996b58a1cfcb1a17c02c6d287f1d4b9e954c12a9
davisherrera <daviseherrera05@gmail.com>
2025-03-07 22:36:31 -0700

    1st Commit

---
+++ Mission09_Herrera/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission09_Herrera.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

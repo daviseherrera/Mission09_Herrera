21dacacf14610af82c0f92a1b1ed176a6f8950b0
daviseherrera <152752724+daviseherrera@users.noreply.github.com>
2025-04-02 08:53:27 -0600

    Delete Mission09_Herrera directory

--- Mission09_Herrera/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Mission09_Herrera.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
